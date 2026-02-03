0ff20ba842845b6e98f31cc35cc293b49744b0ff
kovalev.e <kovalev.e@web-canape.ru>
2026-02-03 17:41:46 +0300

    card

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,14 +1,16 @@
 import useQuiz from "../hooks/useQuiz";
+import { QuizCard } from "./Quiz/QuizCard";
 
 export const Quiz = () => {
   const { constructionTypes, effects } = useQuiz();
+  
   return (
     <>
       <ul>
         <h1>constructionTypes</h1>
 
         {constructionTypes.map((constructionType) => (
-          <li key={constructionType.id}>{constructionType.name}</li>
+          <QuizCard key={constructionType.id} {...constructionType} />
         ))}
       </ul>
 
@@ -16,7 +18,7 @@ export const Quiz = () => {
         <h1>effects</h1>
 
         {effects.map((effect) => (
-          <li key={effect.id}>{effect.name}</li>
+          <QuizCard key={effect.id} {...effect} />
         ))}
       </ul>
     </>
---
+++ src/components/Quiz/QuizCard.jsx
@@ -0,0 +1,13 @@
+export const QuizCard = (props) => {
+  const { id, name, img } = props;
+
+  return (
+    <li className="quiz-item">
+      <input className="quiz-item__checkbox" id={id} type="checkbox" />
+      <label className="quiz-item__label" htmlFor={id}>
+        <img src={img} alt="" width={50} height={50} />
+        {name}
+      </label>
+    </li>
+  );
+};
